daaecae9d6e7d2a406783eff5057efd45756f5c4
James Sinclair <jrsinclair@gmail.com>
2012-06-08 14:40:54 +1000

    Initial commit of numeric arrays package

---
+++ numeric_array.js
@@ -0,0 +1,154 @@
+/**
+ * Mixin functions (aka traits) for numeric arrays
+ */
+/*jslint indent:2, node:true, sloppy:true, nomen:true*/
+(function () {
+  //"use strict";
+  var extend, exported_functions;
+
+  /**
+   * Return the sum of an array
+   */
+  function sum() {
+    function add_next(memo, val) {
+      return memo + val;
+    }
+    return this.reduce(add_next, 0);
+  }
+
+  /**
+   * Calculate the mean of an array
+   */
+  function mean() {
+    return this.sum() / this.length;
+  }
+
+  /**
+   * Population Standard Deviation
+   */
+  function stddevpop() {
+    var mu, len;
+
+    if (this.length === 0) { return NaN; }
+    if (this.length === 1) { return 0; }
+
+    mu  = this.mean();
+    len = this.length;
+    function add_sub_mean_and_square(memo, val) {
+      return memo + (val - mu) * (val - mu);
+    }
+    return Math.sqrt(this.reduce(add_sub_mean_and_square, 0) / len);
+  }
+
+  /**
+   * Sample Standard Deviation
+   */
+  function stddev() {
+    var mu, len;
+
+    if (this.length === 0) { return NaN; }
+    if (this.length === 1) { return 0; }
+
+    mu = this.mean();
+    len = this.length;
+    function add_sub_mean_and_square(memo, val) {
+      return memo + (val - mu) * (val - mu);
+    }
+    return Math.sqrt(this.reduce(add_sub_mean_and_square, 0) / (len - 1));
+  }
+
+  /**
+   * Maximum
+   */
+  function max() {
+    return Math.max.apply(null, this);
+  }
+
+  /**
+   * Minimum
+   */
+  function min() {
+    return Math.min.apply(null, this);
+  }
+
+  /**
+   * Pad
+   *
+   * Pad array to a given length with a given value
+   */
+  function pad(len, val) {
+    while (this.length < len) {
+      this.push(val);
+    }
+  }
+
+  function _padToEqualLengths(arr) {
+    var len;
+
+    // Make sure arrays are equal length
+    this.pad(arr.length, 0);
+    len = this.len;
+    while (arr.length < len) {
+      arr.push(0);
+    }
+    return arr;
+  }
+
+  /**
+   * Add one array to another
+   */
+  function add(arr) {
+
+    // Make sure arrays are equal length
+    arr = _padToEqualLengths.call(this, arr);
+
+    // Add the two arrays
+    function add_together(val, idx) {
+      this[idx] += val;
+    }
+    arr.forEach(add_together, this);
+    return this;
+  }
+
+  /**
+   * Multiply
+   *
+   * Multiply two arrays, element by element
+   */
+  function multiply(arr) {
+
+    // Make sure arrays are equal length
+    arr = _padToEqualLengths.call(this, arr);
+
+    // Multiply the two arrays together
+    function mul_together(val, idx) {
+      this[idx] = val * this[idx];
+    }
+    arr.forEach(mul_together, this);
+    return this;
+  }
+
+  /**
+   * Calculate dot product of two arrays
+   */
+  function dot(arr) {
+    return extend(this.slice(0)).multiply(arr).sum();
+  }
+
+  exported_functions = [
+    sum, mean, stddevpop, stddev, max, min, pad, add, multiply, dot
+  ];
+
+  /**
+   * Extend a given array with mixins
+   */
+  extend = function (arr) {
+    function export_funcs(func) {
+      arr[func.name] = func.bind(arr);
+    }
+    exported_functions.forEach(export_funcs);
+    return arr;
+  };
+
+  exports.extend = extend;
+}());
---
+++ test/testNumericArray.js
@@ -0,0 +1,208 @@
+/**
+ * Test Base Data Set
+ */
+/*jslint indent:2, nomen:false*/
+
+(function () {
+  "use strict";
+  var NA = require('../numeric_array'),
+    exec = require('child_process').exec;
+
+  /**
+   * Extend should add sum function to array's properties
+   */
+  function testExtendShouldAddFunctionsToArray(test) {
+    var arr, funcs;
+    arr = [];
+    funcs = [
+      'sum', 'mean', 'stddevpop', 'stddev', 'max', 'min', 'add', 'pad',
+      'multiply', 'dot'
+    ];
+    NA.extend(arr);
+    function test_func_exists(funcname) {
+      test.ok(
+        arr[funcname],
+        "NumericArray.extend() should add " + funcname + "() to array"
+      );
+    }
+    funcs.forEach(test_func_exists);
+    test.done();
+  }
+  exports['NumericArray.extend() should add functions to array']
+    = testExtendShouldAddFunctionsToArray;
+
+  /**
+   * Sum function should return sum of elements
+   */
+  function testSumShouldReturnSumOfElements(test) {
+    var arr = [1];
+    NA.extend(arr);
+    test.equals(1, arr.sum());
+    arr.push(1);
+    test.equals(2, arr.sum());
+    arr.push(2);
+    test.equals(4, arr.sum());
+    test.done();
+  }
+  exports['sum() should return sum of elements']
+    = testSumShouldReturnSumOfElements;
+
+  /**
+   * Mean function should return mean of elements
+   */
+  function testMeanShouldReturnSumOfElements(test) {
+    var arr = [1];
+    NA.extend(arr);
+    test.equals(1, arr.mean());
+    arr.push(1);
+    test.equals(1, arr.mean());
+    arr.push(4);
+    test.equals(2, arr.mean());
+    test.done();
+  }
+  exports['mean() should return mean of elements']
+    = testMeanShouldReturnSumOfElements;
+
+  function testStdDevPopShouldReturnStdDev(test) {
+    var arr = [1];
+    NA.extend(arr);
+    test.equals(0, arr.stddevpop());
+    arr.push(2);
+    test.equals(0.5, arr.stddevpop());
+    test.done();
+    arr.push(3);
+    test.equals(Math.sqrt(2/3), arr.stddevpop());
+    arr.push(4);
+    test.equals(Math.sqrt(5/4), arr.stddevpop());
+  }
+  exports['stddevpop() should return population standard dev']
+    = testStdDevPopShouldReturnStdDev;
+
+  function testStdDevShouldReturnStdDev(test) {
+    var arr = [1];
+    NA.extend(arr);
+    test.equals(0, arr.stddev());
+    arr.push(2);
+    test.equals(Math.sqrt(1/2), arr.stddev());
+    test.done();
+    arr.push(3);
+    test.equals(1, arr.stddev());
+    arr.push(4);
+    test.equals(Math.sqrt(5/3), arr.stddev());
+  }
+  exports['stddev() should return population standard dev']
+    = testStdDevShouldReturnStdDev;
+
+  function testStdDevFuncsZeroLength(test) {
+    var arr = [];
+    NA.extend(arr);
+    test.ok(isNaN(arr.stddevpop()));
+    test.ok(isNaN(arr.stddev()));
+    test.done();
+  }
+  exports['Standard Dev functions should return NaN on zero length']
+    = testStdDevFuncsZeroLength;
+
+  function testStdDevFuncsUnitLength(test) {
+    var arr = [1];
+    NA.extend(arr);
+    test.equals(arr.stddevpop(), 0);
+    test.equals(arr.stddev(), 0);
+    test.done();
+  }
+  exports['Standard Dev functions should return NaN on zero length']
+    = testStdDevFuncsUnitLength;
+
+  function testMaxShouldReturnArrayMax(test) {
+    var arr = [1, 2, 3, 4];
+    NA.extend(arr);
+    test.equals(arr.max(), 4);
+    arr = NA.extend([-1, -2, -3, -4]);
+    test.equals(arr.max(), -1);
+    test.done();
+  }
+  exports['max() function should return largest integer']
+    = testMaxShouldReturnArrayMax;
+
+  function testMinShouldReturnArrayMin(test) {
+    var arr = [1, 2, 3, 4];
+    NA.extend(arr);
+    test.equals(arr.min(), 1);
+    arr = NA.extend([-1, -2, -3, -4]);
+    test.equals(arr.min(), -4);
+    test.done();
+  }
+  exports['min() function should return largest integer']
+    = testMinShouldReturnArrayMin;
+
+  function testPadChangesArrayLength(test) {
+    var arr = [];
+    NA.extend(arr).pad(5, 0);
+    test.equals(5, arr.length);
+    test.same([0, 0, 0, 0, 0], arr.slice(0));
+    arr = NA.extend([1, 2, 3]);
+    arr.pad(4, -1);
+    test.same([1, 2, 3, -1], arr.slice(0));
+    test.done();
+  }
+  exports['pad() should extend array length to given value']
+    = testPadChangesArrayLength;
+
+  function testAddAddsTwoArrays(test) {
+    var arr, added;
+    arr   = NA.extend([1, 1, 1]);
+    added = arr.add([1, 1, 1]);
+    test.same([2, 2, 2], added.slice(0));
+
+    added = NA.extend([0, 0, 0]).add([1, 1, 1]);
+    test.same([1, 1, 1], added.slice(0));
+
+    added = NA.extend([-1, 0, 1]).add([1, 0, -1]);
+    test.same([0, 0, 0], added.slice(0));
+    test.done();
+  }
+  exports['add() should sum two arrays together'] = testAddAddsTwoArrays;
+
+  function testAddPadsOutWithZeros(test) {
+    var arr;
+    arr = NA.extend([0, 0]).add([0, 0, 0, 0]);
+    test.equals(4, arr.length);
+    test.same([0, 0, 0, 0], arr.slice(0));
+    test.done();
+  }
+  exports['add() should pad with zeros'] = testAddPadsOutWithZeros;
+
+  function testMultiplyMultipliesElements(test) {
+    var arr = NA.extend([1, 2, 3]).multiply([2, 3, 4]);
+    test.same([2, 6, 12], arr.slice(0));
+    arr = NA.extend([1, 1, 1]).multiply([4, 3, 2]);
+    test.same([4, 3, 2], arr.slice(0));
+    arr = NA.extend([2, 1, 4]).multiply([0, 0, 0, 0]);
+    test.same([0, 0, 0, 0], arr.slice(0));
+    test.done();
+  }
+  exports['multiply() should multiply each element together']
+    = testMultiplyMultipliesElements;
+
+  function testDotCalculatesDotProduct(test) {
+    test.equals(4, NA.extend([1, 2]).dot([2, 1]));
+    test.equals(0, NA.extend([1, 0, 0]).dot([0, 1, 1]));
+    test.equals(0, NA.extend([1, -1]).dot([-1, -1]));
+    test.done();
+  }
+  exports['dot() should calculate dot product'] = testDotCalculatesDotProduct;
+
+  function testFilesLintOK(test) {
+    test.expect(1);
+    function check_jslint_result(error, stdout, stderr) {
+      if (error !== null) {
+        console.log('exec error: ' + error);
+      }
+      test.ok((/is\sOK\.\s*$/m).test(stdout), "Errors in jslint:\n" + stdout);
+      test.done();
+    }
+    exec('jslint ' + __dirname + '/../numeric_array.js', check_jslint_result);
+  }
+  exports['File should pass JSLint check']
+    = testFilesLintOK;
+}());
